e063b2a1f009f33b8fd9d98f58b652e1dc51d5f9
matthewsyren <matthew.syren@gmail.com>
2017-05-05 22:00:30 +0200

    Created repository and added HomeActivity, BaseActivity and StockControlActivity

---
+++ OPSC7311_Assign2_15008377/app/src/main/java/a15008377/opsc7311_assign2_15008377/BaseActivity.java
@@ -0,0 +1,82 @@
+package a15008377.opsc7311_assign2_15008377;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by matthew on 2017/02/04.
+ * Class provides a base for the NavigationDrawer that is shared amongst the activities
+ */
+
+public class BaseActivity extends Activity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Declarations
+    private NavigationView navigationView;
+
+    protected void onCreateDrawer() {
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    //Method sets the selected item in the Navigation Drawer
+    public void setSelectedNavItem(int id){
+        navigationView.setCheckedItem(id);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if(id == R.id.nav_stock_control){
+            startActivity(new Intent(getApplicationContext(), StockControlActivity.class));
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
